af53c0260ee3c5bac899ce3b83173e9960c2eb09
Muhammadnoman004 <143292643+Muhammadnoman004@users.noreply.github.com>
2023-10-19 23:27:57 +0500

    First commit

---
+++ app.js
@@ -0,0 +1,34 @@
+function search() {
+    var getuserInp = document.getElementById('inp');
+    var getResultdiv = document.getElementById('resultDiv');
+    var getResultInp = document.getElementById('inp1');
+    var getMesseage = document.getElementById("messeage")
+    var result = document.getElementById("result");
+    
+    if(getuserInp.value == ""){
+        getMesseage.innerText = `Please Fill The Field`
+    }
+    else{
+        
+        var Convert = getuserInp.value.toString();
+        var firstLetter = Convert.slice(0,1).toLowerCase();
+        var AfterFirstLetter = Convert.slice(1).toLowerCase();
+        var Add = firstLetter + AfterFirstLetter;
+        var Split = Add.split("");
+        var Reverse = Split.reverse("")
+        var Join = Reverse.join("");
+
+        getResultdiv.style.display = "block"
+        getResultInp.style.display = "block"
+        getResultInp.value = `${Join}`
+        
+        if (Add == Join) {
+            result.innerText = `"${Add}" is a palindrome word.`;
+        }
+        else {
+            result.innerText = `"${Add}" is not a palindrome word.`;
+        }
+    }
+        
+        getuserInp.value = "";
+    }
